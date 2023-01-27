a92df50c61e3b7619aaacc8f761ca5eff817c942
Ethan <ethan.hawk@valpo.edu>
2023-01-27 14:56:36 -0600

    test.py: remove numpy tests

--- test.py
+++ test.py
@@ -11,17 +11,13 @@ fustal = Futhark(_fustal)
 sepal_length = np.array([5.1,4.9,4.7,4.6,5,5.4,4.6,5,4.4,4.9,5.4,4.8,4.8,4.3,5.8,5.7,5.4,5.1,5.7,5.1,5.4,5.1,4.6,5.1,4.8,5,5,5.2,5.2,4.7,4.8,5.4,5.2,5.5,4.9,5,5.5,4.9,4.4,5.1,5,4.5,4.4,5,5.1,4.8,5.1,4.6,5.3,5,7,6.4,6.9,5.5,6.5,5.7,6.3,4.9,6.6,5.2,5,5.9,6,6.1,5.6,6.7,5.6,5.8,6.2,5.6,5.9,6.1,6.3,6.1,6.4,6.6,6.8,6.7,6,5.7,5.5,5.5,5.8,6,5.4,6,6.7,6.3,5.6,5.5,5.5,6.1,5.8,5,5.6,5.7,5.7,6.2,5.1,5.7,6.3,5.8,7.1,6.3,6.5,7.6,4.9,7.3,6.7,7.2,6.5,6.4,6.8,5.7,5.8,6.4,6.5,7.7,7.7,6,6.9,5.6,7.7,6.3,6.7,7.2,6.2,6.1,6.4,7.2,7.4,7.9,6.4,6.3,6.1,7.7,6.3,6.4,6,6.9,6.7,6.9,5.8,6.8,6.7,6.7,6.3,6.5,6.2,5.9])
 sepal_width = np.array([3.5,3,3.2,3.1,3.6,3.9,3.4,3.4,2.9,3.1,3.7,3.4,3,3,4,4.4,3.9,3.5,3.8,3.8,3.4,3.7,3.6,3.3,3.4,3,3.4,3.5,3.4,3.2,3.1,3.4,4.1,4.2,3.1,3.2,3.5,3.6,3,3.4,3.5,2.3,3.2,3.5,3.8,3,3.8,3.2,3.7,3.3,3.2,3.2,3.1,2.3,2.8,2.8,3.3,2.4,2.9,2.7,2,3,2.2,2.9,2.9,3.1,3,2.7,2.2,2.5,3.2,2.8,2.5,2.8,2.9,3,2.8,3,2.9,2.6,2.4,2.4,2.7,2.7,3,3.4,3.1,2.3,3,2.5,2.6,3,2.6,2.3,2.7,3,2.9,2.9,2.5,2.8,3.3,2.7,3,2.9,3,3,2.5,2.9,2.5,3.6,3.2,2.7,3,2.5,2.8,3.2,3,3.8,2.6,2.2,3.2,2.8,2.8,2.7,3.3,3.2,2.8,3,2.8,3,2.8,3.8,2.8,2.8,2.6,3,3.4,3.1,3,3.1,3.1,3.1,2.7,3.2,3.3,3,2.5,3,3.4,3])
 
-print("numpy---")
-for func in [np.mean, np.var, np.std]:
-    print(f"{func.__name__}: ", func(sepal_length))
+#assert np.mean(sepal_length) == fustal.mean(sepal_length)
+#assert np.var(sepal_length) == fustal.var(sepal_length)
+#assert np.std(sepal_length) == fustal.std(sepal_length)
 
 print("fustal---")
-for func in [fustal.mean, fustal.var, fustal.std, fustal.sample_var, fustal.sample_std]:
+for func in [fustal.stderr, fustal.sample_var, fustal.sample_std, fustal.sample_stderr]:
     print(f"{func.__name__}: ", func(sepal_length))
-
-print("Stderr---")
-print("Fustal: ", fustal.stderr(sepal_length))
-print("Numpy: ", np.std(sepal_length) / np.sqrt(np.size(sepal_length)))
 
 assert fustal.cov(sepal_length, sepal_length) == fustal.var(sepal_length)
 assert fustal.sample_cov(sepal_length, sepal_length) == fustal.sample_var(sepal_length)
