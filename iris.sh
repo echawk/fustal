60993871d3b8a7f00724cf7d269f4eea2aa708ec
Ethan <ethan.hawk@valpo.edu>
2023-01-28 15:54:52 -0600

    iris.sh: add script to generate iris testing data

---
+++ iris.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+cat << EOF | R --no-save > /dev/null
+data('iris')
+write.csv(iris, "iris.csv")
+EOF
+
+sqlite3 "iris.sqlite3" -cmd ".mode csv" ".import --csv iris.csv MAIN"
+
+sqlite3 iris.sqlite3 -cmd "select \"Sepal.Length\" from MAIN" ".exit" |
+      tr '\n' ',' | sed -e "s/^/sepal_length = np.array([/" -e "s/,$/])\n/"
+
+sqlite3 iris.sqlite3 -cmd "select \"Sepal.Width\" from MAIN" ".exit" |
+      tr '\n' ',' | sed -e "s/^/sepal_width = np.array([/" -e "s/,$/])\n/"
+
+for s in setosa versicolor virginica; do
+    sqlite3 iris.sqlite3 -cmd "select \"Sepal.Length\" from MAIN where \"Species\" = \"$s\"" ".exit" |
+          tr '\n' ',' | sed -e "s/^/sepal_length = np.array([/" -e "s/,$/])\n/"
+done
+
+rm iris.sqlite3
+rm iris.csv
