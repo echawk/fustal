0638b28fb0a2a2d360696e9235cb0241d3aec907
Ethan <ethan.hawk@valpo.edu>
2023-02-01 20:11:46 -0600

    test: big overhaul - combine the output of python & R code

--- test.R
+++ test.R
@@ -1,12 +1,9 @@
 data("iris")
 
-t.test(iris$Sepal.Length, mu = 6)
-t.test(iris$Sepal.Length, iris$Sepal.Width)
-
-sd(iris$Sepal.Length)
+s <- sd(iris$Sepal.Length)
 var(iris$Sepal.Length)
-mean(iris$Sepal.Length)
-
-cov(iris$Sepal.Length, iris$Sepal.Width)
-cov(iris$Sepal.Length, iris$Sepal.Length)
+s
+s / sqrt(length(iris$Sepal.Length))
+t.test(iris$Sepal.Length, mu = 6)$statistic[["t"]]
+t.test(iris$Sepal.Length, iris$Sepal.Width)$statistic[["t"]]
 cor(iris$Sepal.Length, iris$Sepal.Width)
--- test.py
+++ test.py
@@ -9,27 +9,22 @@ fustal = Futhark(_fustal)
 sepal_length = np.array([5.1,4.9,4.7,4.6,5,5.4,4.6,5,4.4,4.9,5.4,4.8,4.8,4.3,5.8,5.7,5.4,5.1,5.7,5.1,5.4,5.1,4.6,5.1,4.8,5,5,5.2,5.2,4.7,4.8,5.4,5.2,5.5,4.9,5,5.5,4.9,4.4,5.1,5,4.5,4.4,5,5.1,4.8,5.1,4.6,5.3,5,7,6.4,6.9,5.5,6.5,5.7,6.3,4.9,6.6,5.2,5,5.9,6,6.1,5.6,6.7,5.6,5.8,6.2,5.6,5.9,6.1,6.3,6.1,6.4,6.6,6.8,6.7,6,5.7,5.5,5.5,5.8,6,5.4,6,6.7,6.3,5.6,5.5,5.5,6.1,5.8,5,5.6,5.7,5.7,6.2,5.1,5.7,6.3,5.8,7.1,6.3,6.5,7.6,4.9,7.3,6.7,7.2,6.5,6.4,6.8,5.7,5.8,6.4,6.5,7.7,7.7,6,6.9,5.6,7.7,6.3,6.7,7.2,6.2,6.1,6.4,7.2,7.4,7.9,6.4,6.3,6.1,7.7,6.3,6.4,6,6.9,6.7,6.9,5.8,6.8,6.7,6.7,6.3,6.5,6.2,5.9])
 sepal_width = np.array([3.5,3,3.2,3.1,3.6,3.9,3.4,3.4,2.9,3.1,3.7,3.4,3,3,4,4.4,3.9,3.5,3.8,3.8,3.4,3.7,3.6,3.3,3.4,3,3.4,3.5,3.4,3.2,3.1,3.4,4.1,4.2,3.1,3.2,3.5,3.6,3,3.4,3.5,2.3,3.2,3.5,3.8,3,3.8,3.2,3.7,3.3,3.2,3.2,3.1,2.3,2.8,2.8,3.3,2.4,2.9,2.7,2,3,2.2,2.9,2.9,3.1,3,2.7,2.2,2.5,3.2,2.8,2.5,2.8,2.9,3,2.8,3,2.9,2.6,2.4,2.4,2.7,2.7,3,3.4,3.1,2.3,3,2.5,2.6,3,2.6,2.3,2.7,3,2.9,2.9,2.5,2.8,3.3,2.7,3,2.9,3,3,2.5,2.9,2.5,3.6,3.2,2.7,3,2.5,2.8,3.2,3,3.8,2.6,2.2,3.2,2.8,2.8,2.7,3.3,3.2,2.8,3,2.8,3,2.8,3.8,2.8,2.8,2.6,3,3.4,3.1,3,3.1,3.1,3.1,2.7,3.2,3.3,3,2.5,3,3.4,3])
 
-#assert np.mean(sepal_length) == fustal.mean(sepal_length)
-#assert np.var(sepal_length) == fustal.var(sepal_length)
-#assert np.std(sepal_length) == fustal.std(sepal_length)
-
-print("fustal---")
-for func in [fustal.stderr, fustal.sample_var, fustal.sample_std, fustal.sample_stderr]:
-    print(f"{func.__name__}: ", func(sepal_length))
+# funcs not printed: fustal.stderr, fustal.mean, fustal.var, fustal.std
+for func in [fustal.sample_var, fustal.sample_std, fustal.sample_stderr]:
+    print(func(sepal_length))
 
 assert fustal.cov(sepal_length, sepal_length) == fustal.var(sepal_length)
 assert fustal.sample_cov(sepal_length, sepal_length) == fustal.sample_var(sepal_length)
 
-print("1 Sample T-Test---")
-print("Fustal: ", fustal.one_sample_t_test(sepal_length, 6))
+print(fustal.one_sample_t_test(sepal_length, 6))
 
 for func in [fustal.two_sample_t_test, fustal.pearson_correlation_coefficient]:
-    print(f"{func.__name__}: ", func(sepal_length, sepal_width))
+    print(func(sepal_length, sepal_width))
 
 # Data to compute ANOVA
 sepal_length_setosa = np.array([5.1,4.9,4.7,4.6,5,5.4,4.6,5,4.4,4.9,5.4,4.8,4.8,4.3,5.8,5.7,5.4,5.1,5.7,5.1,5.4,5.1,4.6,5.1,4.8,5,5,5.2,5.2,4.7,4.8,5.4,5.2,5.5,4.9,5,5.5,4.9,4.4,5.1,5,4.5,4.4,5,5.1,4.8,5.1,4.6,5.3,5])
 sepal_length_versicolor = np.array([7,6.4,6.9,5.5,6.5,5.7,6.3,4.9,6.6,5.2,5,5.9,6,6.1,5.6,6.7,5.6,5.8,6.2,5.6,5.9,6.1,6.3,6.1,6.4,6.6,6.8,6.7,6,5.7,5.5,5.5,5.8,6,5.4,6,6.7,6.3,5.6,5.5,5.5,6.1,5.8,5,5.6,5.7,5.7,6.2,5.1,5.7])
 sepal_length_virginica = np.array([6.3,5.8,7.1,6.3,6.5,7.6,4.9,7.3,6.7,7.2,6.5,6.4,6.8,5.7,5.8,6.4,6.5,7.7,7.7,6,6.9,5.6,7.7,6.3,6.7,7.2,6.2,6.1,6.4,7.2,7.4,7.9,6.4,6.3,6.1,7.7,6.3,6.4,6,6.9,6.7,6.9,5.8,6.8,6.7,6.7,6.3,6.5,6.2,5.9])
 
-print("F-Test---")
-print("Fustal: ", fustal.f_test(sepal_length_setosa, sepal_length_virginica))
+#print(fustal.f_test(sepal_length_setosa, sepal_length_virginica))
+assert fustal.f_test(sepal_length_setosa, sepal_length_virginica) == (lambda x: x * x)(fustal.two_sample_t_test(sepal_length_setosa, sepal_length_virginica))
