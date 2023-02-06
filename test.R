6c46fc8f6f2e518bca3ca2283c867130238c6125
Ethan <ethan.hawk@valpo.edu>
2023-02-05 19:07:04 -0600

    test.{R,py}: add test for simple linear regression

--- test.R
+++ test.R
@@ -7,6 +7,7 @@ s / sqrt(length(iris$Sepal.Length))
 t.test(iris$Sepal.Length, mu = 6)$statistic[["t"]]
 t.test(iris$Sepal.Length, iris$Sepal.Width)$statistic[["t"]]
 cor(iris$Sepal.Length, iris$Sepal.Width)
+coefficients(lm(Sepal.Width ~ Sepal.Length, data=iris))
 anova(aov(Sepal.Length ~ Species, data=iris))
 chisq_data <- matrix(c(120, 90, 40, 110, 95, 45), ncol=3, byrow=TRUE)
 chisq.test(chisq_data)
