592dc761ae4d917e359322292ebf6cb189663f34
Ethan <ethan.hawk@valpo.edu>
2023-02-11 16:03:46 -0600

    test{,.R,.py}: refactor

--- test.py
+++ test.py
@@ -18,8 +18,13 @@ assert fustal.sample_cov(sepal_length, sepal_length) == fustal.sample_var(sepal_
 
 print(fustal.one_sample_t_test(sepal_length, 6))
 
-for func in [fustal.two_sample_t_test, fustal.pearson_correlation_coefficient, fustal.simple_linear_regression, fustal.wilcoxon_rank_sum_test]:
-    print(func(sepal_length, sepal_width))
+for func in [fustal.two_sample_t_test, fustal.pearson_correlation_coefficient, fustal.simple_linear_regression]:
+    v = func(sepal_length, sepal_width)
+    if type(v) is tuple:
+        print(v[0])
+        print(v[1])
+    else:
+        print(v)
 
 # Data to compute ANOVA
 sepal_length_setosa = np.array([5.1,4.9,4.7,4.6,5,5.4,4.6,5,4.4,4.9,5.4,4.8,4.8,4.3,5.8,5.7,5.4,5.1,5.7,5.1,5.4,5.1,4.6,5.1,4.8,5,5,5.2,5.2,4.7,4.8,5.4,5.2,5.5,4.9,5,5.5,4.9,4.4,5.1,5,4.5,4.4,5,5.1,4.8,5.1,4.6,5.3,5])
