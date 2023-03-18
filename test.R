3f2cdcd297272c7b912f39a1347a97a0003522a3
Ethan <ethan.hawk@valpo.edu>
2023-03-18 02:27:45 -0500

    test*: completely revamp test suite

--- test.R
+++ test.R
@@ -1,13 +1,22 @@
 data("iris")
 
 s <- sd(iris$Sepal.Length)
+"sample_var"
 var(iris$Sepal.Length)
+"sample_std"
 s
+"sample_stderr"
 s / sqrt(length(iris$Sepal.Length))
+"one_sample_t_test"
 t.test(iris$Sepal.Length, mu = 6)$statistic[["t"]]
+"two_sample_t_test"
 t.test(iris$Sepal.Length, iris$Sepal.Width)$statistic[["t"]]
+"pearson_correlation_coefficient"
 cor(iris$Sepal.Length, iris$Sepal.Width)
+"simple_linear_regression"
 as.vector(coefficients(lm(Sepal.Width ~ Sepal.Length, data=iris)))
+"f_test"
 as.vector(anova(aov(Sepal.Length ~ Species, data=iris))$"F value")[1]
 chisq_data <- matrix(c(120, 90, 40, 110, 95, 45), ncol=3, byrow=TRUE)
+"chi_squared_test"
 as.vector(chisq.test(chisq_data)$statistic)
--- test.py
+++ test.py
@@ -9,22 +9,27 @@ fustal = Futhark(_fustal)
 sepal_length = np.array([5.1,4.9,4.7,4.6,5,5.4,4.6,5,4.4,4.9,5.4,4.8,4.8,4.3,5.8,5.7,5.4,5.1,5.7,5.1,5.4,5.1,4.6,5.1,4.8,5,5,5.2,5.2,4.7,4.8,5.4,5.2,5.5,4.9,5,5.5,4.9,4.4,5.1,5,4.5,4.4,5,5.1,4.8,5.1,4.6,5.3,5,7,6.4,6.9,5.5,6.5,5.7,6.3,4.9,6.6,5.2,5,5.9,6,6.1,5.6,6.7,5.6,5.8,6.2,5.6,5.9,6.1,6.3,6.1,6.4,6.6,6.8,6.7,6,5.7,5.5,5.5,5.8,6,5.4,6,6.7,6.3,5.6,5.5,5.5,6.1,5.8,5,5.6,5.7,5.7,6.2,5.1,5.7,6.3,5.8,7.1,6.3,6.5,7.6,4.9,7.3,6.7,7.2,6.5,6.4,6.8,5.7,5.8,6.4,6.5,7.7,7.7,6,6.9,5.6,7.7,6.3,6.7,7.2,6.2,6.1,6.4,7.2,7.4,7.9,6.4,6.3,6.1,7.7,6.3,6.4,6,6.9,6.7,6.9,5.8,6.8,6.7,6.7,6.3,6.5,6.2,5.9])
 sepal_width = np.array([3.5,3,3.2,3.1,3.6,3.9,3.4,3.4,2.9,3.1,3.7,3.4,3,3,4,4.4,3.9,3.5,3.8,3.8,3.4,3.7,3.6,3.3,3.4,3,3.4,3.5,3.4,3.2,3.1,3.4,4.1,4.2,3.1,3.2,3.5,3.6,3,3.4,3.5,2.3,3.2,3.5,3.8,3,3.8,3.2,3.7,3.3,3.2,3.2,3.1,2.3,2.8,2.8,3.3,2.4,2.9,2.7,2,3,2.2,2.9,2.9,3.1,3,2.7,2.2,2.5,3.2,2.8,2.5,2.8,2.9,3,2.8,3,2.9,2.6,2.4,2.4,2.7,2.7,3,3.4,3.1,2.3,3,2.5,2.6,3,2.6,2.3,2.7,3,2.9,2.9,2.5,2.8,3.3,2.7,3,2.9,3,3,2.5,2.9,2.5,3.6,3.2,2.7,3,2.5,2.8,3.2,3,3.8,2.6,2.2,3.2,2.8,2.8,2.7,3.3,3.2,2.8,3,2.8,3,2.8,3.8,2.8,2.8,2.6,3,3.4,3.1,3,3.1,3.1,3.1,2.7,3.2,3.3,3,2.5,3,3.4,3])
 
+def sanitize(func_name: str):
+    new_name = func_name.replace("futhark_entry_", "")
+    return new_name
+
 # funcs not printed: fustal.stderr, fustal.mean, fustal.var, fustal.std
 for func in [fustal.sample_var, fustal.sample_std, fustal.sample_stderr]:
-    print(func(sepal_length))
+    print("py", sanitize(func.__name__), func(sepal_length), sep=",")
 
 assert fustal.cov(sepal_length, sepal_length) == fustal.var(sepal_length)
 assert fustal.sample_cov(sepal_length, sepal_length) == fustal.sample_var(sepal_length)
 
-print(fustal.one_sample_t_test(sepal_length, 6))
+print("py", sanitize(fustal.one_sample_t_test.__name__),
+      fustal.one_sample_t_test(sepal_length, 6), sep=",")
 
 for func in [fustal.two_sample_t_test, fustal.pearson_correlation_coefficient, fustal.simple_linear_regression]:
     v = func(sepal_length, sepal_width)
     if type(v) is tuple:
-        print(v[0])
-        print(v[1])
+        print("py", sanitize(func.__name__), ','.join(map(str, list(v))), sep=",")
     else:
-        print(v)
+        print("py", sanitize(func.__name__), v, sep=",")
+
 
 # Data to compute ANOVA
 sepal_length_setosa = np.array([5.1,4.9,4.7,4.6,5,5.4,4.6,5,4.4,4.9,5.4,4.8,4.8,4.3,5.8,5.7,5.4,5.1,5.7,5.1,5.4,5.1,4.6,5.1,4.8,5,5,5.2,5.2,4.7,4.8,5.4,5.2,5.5,4.9,5,5.5,4.9,4.4,5.1,5,4.5,4.4,5,5.1,4.8,5.1,4.6,5.3,5])
@@ -32,10 +37,12 @@ sepal_length_versicolor = np.array([7,6.4,6.9,5.5,6.5,5.7,6.3,4.9,6.6,5.2,5,5.9,
 sepal_length_virginica = np.array([6.3,5.8,7.1,6.3,6.5,7.6,4.9,7.3,6.7,7.2,6.5,6.4,6.8,5.7,5.8,6.4,6.5,7.7,7.7,6,6.9,5.6,7.7,6.3,6.7,7.2,6.2,6.1,6.4,7.2,7.4,7.9,6.4,6.3,6.1,7.7,6.3,6.4,6,6.9,6.7,6.9,5.8,6.8,6.7,6.7,6.3,6.5,6.2,5.9])
 
 sepal_length_matr = np.matrix([sepal_length_setosa, sepal_length_virginica, sepal_length_versicolor])
-print(fustal.f_test(sepal_length_matr))
+print("py", sanitize(fustal.f_test.__name__), fustal.f_test(sepal_length_matr), sep=",")
+
 # TODO: see why this is wrong?
 #assert fustal.f_test(sepal_matr_setosa_virginica) == (lambda x: x * x)(fustal.two_sample_t_test(sepal_length_setosa, sepal_length_virginica))
 
 # FIXME: try using some built in data from R for this
 chisq_data = np.matrix([[120, 90, 40], [110, 95, 45]])
-print(fustal.chi_squared_test(chisq_data));
+print("py", sanitize(fustal.chi_squared_test.__name__), fustal.chi_squared_test(chisq_data), sep=",")
+
