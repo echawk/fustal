282af9c760009ac6e5e5e0f2676cbade5a8f3851
Ethan <ethan.hawk@valpo.edu>
2023-04-23 00:41:58 -0500

    utils/timer.py: refactor

--- utils/timer.py
+++ utils/timer.py
@@ -1,7 +1,13 @@
 import sys
 import subprocess
 from datetime import datetime
-t = datetime.now()
-subprocess.call(sys.argv[1:])
-elapsed_time = datetime.now() - t
-print("real " + str(elapsed_time.seconds + elapsed_time.microseconds / 1000000) + "s", file=sys.stderr)
+
+def timer(l):
+    "Returns the time elapsed to run subprocess 'l' in seconds."
+    t = datetime.now()
+    subprocess.call(l)
+    elapsed_time = datetime.now() - t
+    return elapsed_time.seconds + elapsed_time.microseconds / 1000000
+
+if __name__ == "__main__":
+    print("real " + str(timer(sys.argv[1:])) + "s", file=sys.stderr)
