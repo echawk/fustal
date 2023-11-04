ad95ff45b1e4414c3f9770b2f22eb2d71306867d
Ethan <ethan.hawk@valpo.edu>
2023-11-04 11:52:50 -0500

    utils/gendocs.el: rewrite gendocs in elisp

---
+++ utils/gendocs.el
@@ -0,0 +1,31 @@
+#!/usr/bin/emacs --script
+
+;; Emacs lisp version of gendocs.
+
+(setq source-file (nth 0 argv))
+
+(message "\\documentclass{article}\n
+\\usepackage{amsmath}\n
+\\newcommand{\\divider}{\\par\\noindent\\rule{\\textwidth}{1pt}}\n
+\\begin{document}\n")
+
+(message
+ (with-temp-buffer
+   (insert-file-contents source-file)
+   (keep-lines (rx bol (or (: "-- " (or "desc" "equation"))
+                           "entry")))
+   (replace-regexp-in-region (rx "-- desc: " (group-n 1 (0+ any) eol))
+                             "\n\\\\noindent\nDescription:\n\n \\1 \n\n")
+   (replace-regexp-in-region
+    (rx "-- equation: " (group-n 1 (+ any) eol))
+    "\\\\begin{center}\n \\1 \n\\\\end{center}")
+
+   (replace-regexp-in-region
+    (rx bol "entry" (1+ " ") (group-n 1 (1+ (not " "))) (1+ " ") (group-n 2 (1+ any)) "=")
+    "\n\\\\divider\n
+\\\\noindent\nFunction Name: \\\\begin{verbatim} \\1 \\\\end{verbatim}\n
+\\\\noindent\nFunction Type Signature: \\\\begin{verbatim} \\2 \\\\end{verbatim}\n")
+
+   (delete-matching-lines (rx bol "--"))
+   (buffer-substring-no-properties (point-min) (point-max))))
+(message"\n\\end{document}")
