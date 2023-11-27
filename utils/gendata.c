6b2e5c694d33f67ed1f55b4487feb041550cddc7
Ethan <ethan.hawk@valpo.edu>
2023-11-27 12:29:11 -0600

    utils/gendata.c: add c code to generate large csvs of random data

---
+++ utils/gendata.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_ITERS 5E5
+#define ROWS 3
+
+int main() {
+  int i, j;
+  srand(1);
+  FILE *fptr = fopen("gendata-csv", "w");
+  for (i = 0; i < ROWS; i++) {
+    for (j = 0; j < MAX_ITERS - 1; j++) {
+      fprintf(fptr, "%f,", (double)rand() / RAND_MAX);
+    }
+    fprintf(fptr, "%f", (double)rand() / RAND_MAX);
+    fprintf(fptr, "\n");
+  }
+  fclose(fptr);
+  return 0;
+}
